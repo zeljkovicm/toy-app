4accfc01687782c656f47e17dcb5118bebe72bf1
zeljkovicm <zeljkovic_milan@yahoo.com>
2025-12-13 17:07:21 +0100

    Adding getInitials to auth store since it is more commonly used.

--- src/app/auth-store.ts
+++ src/app/auth-store.ts
@@ -95,6 +95,14 @@ export const AuthStore = signalStore(
             if (token && user) {
                 patchState(store, { user, accessToken: token })
             }
+        },
+        getInitials(name: string | undefined): string {
+            if (!name) return '';
+            const parts = name.trim().split(' ');
+            if (parts.length === 1) {
+                return parts[0].charAt(0).toUpperCase();
+            }
+            return (parts[0].charAt(0) + parts[parts.length - 1].charAt(0)).toUpperCase();
         }
     }))
 )
